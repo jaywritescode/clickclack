716bedfb5a3ca832ef1083eb04d89eec90eaecdd
Jay Harris <jaywritescode@users.noreply.github.com>
2020-08-03 09:45:25 -0400

    add replay function

--- index.js
+++ index.js
@@ -22,3 +22,43 @@ const ClickClackReceiver = (el, callback) => {
     return Date.now() - startTime;
   }
 };
+
+const InputTypes = {
+  INSERT: 'insertText',
+  DELETE: 'deleteContentBackwards',
+};
+
+const replay = (parent, events) => {
+  if (!events.length) {
+    return;
+  }
+
+  const actions = {
+    [InputTypes.INSERT]: appendText,
+    [InputTypes.DELETE]: chopText,
+  };
+
+  function appendText(text) {
+    parent.textContent += text;
+  }
+
+  function chopText(n = 1) {
+    parent.textContent = parent.textContent.slice(0, -n);
+  }
+
+  function applyEvent({ data, inputType }) {
+    actions[inputType].call(null, data);
+  }
+
+  (function doReplay(_events) {
+    const [current, next, ...rest] = events;
+    if (!current) {
+      return;
+    }
+
+    applyEvent(current);
+    if (next) {
+      setTimeout(() => doReplay([next, ...rest]), next.timestamp - current.timestamp);
+    }
+  })(events);
+};
