a39b3a57bd9bc8f71c837a7600c94ddd5a535750
Jay Harris <jaywritescode@users.noreply.github.com>
2020-08-09 12:52:44 -0400

    reset button should reset all three text "zones"

--- site/index.js
+++ site/index.js
@@ -1,30 +1,37 @@
 import replay, { Recording } from '../index';
 
 document.addEventListener('DOMContentLoaded', () => {
-  var codemirror = CodeMirror.fromTextArea(document.getElementById('output'), {
+  const codemirror = CodeMirror.fromTextArea(document.getElementById('output'), {
     mode: 'json',
     readOnly: true,
     lineNumbers: true,
     lineWrapping: true,
   });
 
-  const recorder = Recording(document.getElementById('typing-area'), (history) => {
+  const typingElement = document.querySelector('#typing-area textarea');
+  const replayElement = document.querySelector('#replay-area .text-holder');
+
+  const recorder = Recording(typingElement, (history) => {
     codemirror.setValue(JSON.stringify(history, null, '  '));
   });
   recorder.init();
 
   document.querySelector('#typing-area button').addEventListener('click', () => {
     recorder.destroy();
+
+    typingElement.value = null;
+    clearReplay();
+
     recorder.init();
-  })
+  });
 
   document.querySelector('#replay-area button').addEventListener('click', () => {
-    const parent = document.querySelector('#replay-area .text-holder');
-    const events = JSON.parse(codemirror.getValue());
-
-    while (parent.firstChild) {
-      parent.removeChild(parent.firstChild);
-    }
-    replay(parent, events);
+    replay(replayElement, JSON.parse(codemirror.getValue()));
   });
+
+  function clearReplay() {
+    while (replayElement.firstChild) {
+      replayElement.removeChild(replayElement.firstChild);
+    }
+  }
 });
